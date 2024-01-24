86f661a3c8acdcc5edb3a48783c6f371d69937a1
karthik <dk.karthikd9@gmail.com>
2024-01-24 20:50:23 +0530

    client:registration page created with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Registration from './components/Registration'
 
 const App = () => {
   return (
     <div className='container'>
       <h1>App</h1>
+      <Registration></Registration>
     </div>
   )
 }
